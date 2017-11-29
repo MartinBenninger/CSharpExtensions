941e578de275076900719665c8505a6464a78d02
Martin Benninger <martin.benninger@outlook.com>
2017-11-29 15:03:49 +0100

    Add a generic conversion extension method
    Allows for running a Converter delegate as an extension method.

--- CSharpExtensions/CSharpExtensions/ConversionExtensions.cs
+++ CSharpExtensions/CSharpExtensions/ConversionExtensions.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace CSharpExtensions
+{
+    using System;
+
+    public static class ConversionExtensions
+    {
+        public static TOut Convert<TIn, TOut>(this TIn input, Converter<TIn, TOut> converter)
+        {
+            return converter(input);
+        }
+    }
+}
